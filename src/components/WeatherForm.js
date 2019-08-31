9bb79be1c3c520f4d1a4cb2ee6747adf97ff52b8
Jeff Horner <jhorner3208@gmail.com>
2019-08-31 08:07:33 -0500

    refactored some code and added a reset button for the user to reset the application once they find their weather

--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -1,42 +1,53 @@
-import React, { Component } from 'react';
-import '../styles/WeatherForm.css';
+import React, { Component } from "react";
+import "../styles/WeatherForm.css";
 
 class WeatherForm extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      city: '',
-      country: ''
-    }
+      city: "",
+      country: ""
+    };
     this.onInputChange = this.onInputChange.bind(this);
   }
 
   onInputChange(event) {
     this.setState({
       [event.target.name]: event.target.value
-    })
+    });
   }
 
   render() {
-
     return (
-      <form onSubmit={this.props.getWeather}>
-        <input className='input' 
+      <form>
+        <input
+          className="input"
           value={this.state.city}
           onChange={this.onInputChange}
-          type='text' 
-          placeholder='city...' 
-          name='city' 
+          type="text"
+          placeholder="city..."
+          name="city"
         />
-        <input className='input' 
+        <input
+          className="input"
           value={this.state.country}
           onChange={this.onInputChange}
-          type='text' 
-          placeholder='country...' 
-          name='country'
+          type="text"
+          placeholder="country..."
+          name="country"
         />
-        <button className='get-weather'>Get Weather</button>
+        <button
+            className="get-weather"
+            type="submit"
+            onClick={(e) => {
+                e.preventDefault();
+                this.props.fetchedData
+                    ? this.props.resetWeather()
+                    : this.props.getWeather(this.state.city, this.state.country);
+            }}
+        >
+          {this.props.fetchedData ? "Reset" : "Get Weather"}
+        </button>
       </form>
     );
   }
